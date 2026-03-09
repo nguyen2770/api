5e7f6d794133b2a9d1d88273b0efa7f5d6680db5
ThanhNamNg <thanhnam052004@gmail.com>
2026-03-09 23:43:44 +0700

    feat(db): create model assets

---
+++ src/models/index.js
@@ -0,0 +1,22 @@
+module.exports.Manufacturer = require('./assets/manufacturer.model');
+module.exports.Category = require('./assets/category.model');
+module.exports.SubCategory = require('./assets/subCategory.model');
+module.exports.Asset = require('./assets/asset.model');
+module.exports.SpareCategoryModel = require('./assets/spareCategory.model');
+module.exports.SpareSubCategoryModel = require('./assets/spareSubCategory.model');
+
+module.exports.AssetModelMonitoringPoint = require('./assets/assetModelMonitoringPoint.model');
+module.exports.AssetModelSparePart = require('./assets/assetModelSparePart.model');
+module.exports.ServiceCategoryModel = require('./assets/serviceCategory.model');
+module.exports.ServiceSubCategoryModel = require('./assets/serviceSubCategory.model');
+
+module.exports.AssetModelParameterModel = require('./assets/assetModelParameter.model');
+module.exports.AssetTypeModel = require('./assets/assetType.model');
+module.exports.AssetModelFailureTypeModel = require('./assets/assetModelFailureType.model');
+module.exports.AssetModelSeftDiagnosiaModel = require('./assets/assetModelSeftDiagnosia.model');
+module.exports.AssetModelSeftDiagnosiaAnswerValueModel = require('./assets/assetModelSeftDiagnosiaAnswerValue.model');
+module.exports.AssetModelSeftDiagnosiaTagModel = require('./assets/assetModelSeftDiagnosiaTag.model');
+module.exports.AssetModelSolutionModel = require('./assets/assetModelSolution.model');
+module.exports.AssetModelSolutionTagModel = require('./assets/assetModelSolutionTag.model');
+
+module.exports.AssetTypeModel = require('./assets/assetType.model');
